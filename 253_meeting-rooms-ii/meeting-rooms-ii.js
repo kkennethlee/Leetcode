b3dc4f4322c6d7ff931203976d7a2af058d7b19b
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2020-11-07 13:48:44 -0800

    #253 more efficient solution

--- 253_meeting-rooms-ii/meeting-rooms-ii.js
+++ 253_meeting-rooms-ii/meeting-rooms-ii.js
@@ -1,10 +1,15 @@
 /*
 #253 https://leetcode.com/problems/meeting-rooms
+*/
+
+/*
 Time: O(NLog(N) + N^2) -> O(N^2)
 Space: O(N)
 */
+function _minMeetingRooms(intervals) {  
+  if(intervals.length === 0) return 0;
+  if(intervals.length === 1) return 1;
 
-function _minMeetingRooms(intervals) {
 
   intervals.sort( (a, b) => a[0] > b[0] );
 
@@ -28,6 +33,50 @@ function _minMeetingRooms(intervals) {
 
 
   return rooms.length;
+
+}
+
+/*
+Time: O(N + 2 * NLog(N) + N) -> O(NLog(N))
+Space: O(2N) -> O(N)
+*/
+function minMeetingRooms(intervals) {
+  if(intervals.length <= 1) return intervals.length;
+
+  const startTime = [];
+  const endTime = [];
+
+  for(let i = 0; i < intervals.length; i++) {
+
+    startTime.push( intervals[i][0] );
+    endTime.push( intervals[i][1] );
+
+  }
+
+  startTime.sort( (a, b) => a > b );
+  endTime.sort( (a, b) => a > b );
+
+  let startIndex = 0;
+  let endIndex = 0;
+  let rooms = 0;
+  let concurrentMeetings = 0;
+  while( startIndex < intervals.length ) {
+
+    if(startTime[startIndex] < endTime[endIndex]) {
+      concurrentMeetings++;
+      startIndex++;
+    } else {
+      concurrentMeetings--;
+      endIndex++;
+    }
+
+    if(concurrentMeetings > rooms) {
+      rooms = concurrentMeetings;
+    }
+  }
+
+  return rooms;
+
 
 }
 module.exports = {minMeetingRooms};
--- 253_meeting-rooms-ii/meeting-rooms-ii.test.js
+++ 253_meeting-rooms-ii/meeting-rooms-ii.test.js
@@ -6,9 +6,13 @@ test("example 1", () => {
 })
 
 test("example 2", () => {
-  expect(minMeetingRooms([[0, 30], [5, 10], [15, 20], [17, 25], [30, 40]])).toEqual(3); //2
+  expect(minMeetingRooms([[7, 10], [2, 4]])).toEqual(1);
 })
 
 test("example 3", () => {
+  expect(minMeetingRooms([[0, 30], [5, 10], [15, 20], [17, 25], [30, 40]])).toEqual(3);
+})
+
+test("example 4", () => {
   expect(minMeetingRooms([[0, 6], [5, 10], [8, 20]])).toEqual(2);
 })
