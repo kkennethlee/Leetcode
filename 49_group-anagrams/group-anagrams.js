f25d99e7fa9d0a382fcfd27284923cdab0992db3
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2020-11-11 18:08:38 -0800

    #49 use more efficient(?) method

--- 49_group-anagrams/group-anagrams.js
+++ 49_group-anagrams/group-anagrams.js
@@ -7,7 +7,7 @@ Brute force
 Time: O(N * MLog(M)) N: number of words in array, M: length of longest word
 Space: O(N)
 */
-function groupAnagrams(strs) {
+function _groupAnagrams(strs) {
   const hash = {};
 
   for(let i = 0; i < strs.length; i++) {
@@ -19,6 +19,43 @@ function groupAnagrams(strs) {
     }
     hash[sorted].push(word);
   }
+  const values = Object.values(hash);
+  return values;
+}
+
+/*
+More efficient(?) method: get rid of sort function
+Time: O(N*M)
+Space: O(N)
+*/
+function groupAnagrams(strs) {
+  const getCharIndex = (char) => char.charCodeAt(0) - 97;
+
+  const getCharacterArrays = (word) => {
+    const array = new Array(26).fill(0);
+
+    for(let i = 0; i < word.length; i++) {
+      const char = word[i];
+      const index = getCharIndex(char);
+      array[index]++;
+    }
+
+    return array;
+  }
+
+  const hash = {};
+
+  for(let i = 0; i < strs.length; i++) {
+    const word = strs[i].toLowerCase();
+    const array = getCharacterArrays(word);
+
+    if(!hash.hasOwnProperty(array)) {
+      hash[array] = [];
+    }
+
+    hash[array].push(word);
+  }
+
   const values = Object.values(hash);
   return values;
 }
