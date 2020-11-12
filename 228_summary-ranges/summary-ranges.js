db6706400eb5c7192057cc5046ea35759571f29d
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2020-11-11 23:42:07 -0800

    #228 solution with time limit exceeded

---
+++ 228_summary-ranges/summary-ranges.js
@@ -0,0 +1,57 @@
+/*
+#228 https://leetcode.com/problems/summary-ranges 
+Time: O(N)
+Space: O(N)
+*/
+
+
+function summaryRanges(nums) {
+  if(!nums.length) return [];
+  if(nums.length === 1) return [nums[0] + ""];
+
+  let start = nums[0];
+  let end = nums[ nums.length - 1 ];
+
+  const hash = {};
+  let output = [];
+
+  for(const num of nums) {
+    if(!hash.hasOwnProperty(num)) {
+      hash[num] = true;
+    }
+  }
+
+  let beginRange = null;
+  let endRange = null;
+
+  for(let i = start; i <= end; i++) {
+    if(beginRange === null && hash[i]) {
+      beginRange = i;
+    }
+
+    if( beginRange !== null && (!hash[i] || i === end)) {
+      if(i === end) {
+        endRange = end;
+      } else {
+        endRange = i - 1;
+      }
+
+      let range = null;
+      if(endRange === beginRange) {
+        range = "" + beginRange;
+      } else {
+        range = `${beginRange}->${endRange}`
+      }
+
+      output.push(range);
+      beginRange = null;
+    }
+
+
+  }
+
+  return output;
+}
+
+
+module.exports = {summaryRanges}
---
+++ 228_summary-ranges/summary-ranges.test.js
@@ -0,0 +1,58 @@
+const {summaryRanges} = require('./summary-ranges');
+
+/*
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+*/
+test("example 1", () => {
+  expect(summaryRanges([0,1,2,4,5,7])).toEqual(["0->2","4->5","7"]);
+});
+
+/*
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+*/
+test("example 2", () => {
+  expect(summaryRanges([0,2,3,4,6,8,9])).toEqual(["0","2->4","6","8->9"]);
+});
+
+/*
+Input: nums = []
+Output: []
+*/
+test("example 3", () => {
+  expect(summaryRanges([])).toEqual([]);
+});
+
+/*
+Input: nums = [-1]
+Output: ["-1"]
+*/
+test("example 4", () => {
+  expect(summaryRanges([-1])).toEqual(["-1"]);
+});
+
+/*
+Input: nums = [0]
+Output: ["0"]
+*/
+test("example 5", () => {
+  expect(summaryRanges([0])).toEqual(["0"]);
+});
+
+test("example 6", () => {
+  expect(summaryRanges([-2,-1,0,2,3,4,6,8,9])).toEqual(["-2->0","2->4","6","8->9"]);
+});
+
+test("example 7", () => {
+  expect(summaryRanges([0,9])).toEqual(["0","9"]);
+})
