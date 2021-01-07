36878dcf5f12d4aea3ae390b9561a32038ab7b38
Kenneth Lee <kyung.kenneth.lee@gmail.com>
2021-01-06 22:09:05 -0800

    verified #45

--- 45_jump-game-ii/jump-game-ii.js
+++ 45_jump-game-ii/jump-game-ii.js
@@ -1,13 +1,13 @@
 /*
-#45 https://leetcode.com/problems/jump-game-ii
+#45 https://leetcode.com/problems/jump-game-ii (verified)
 */
 
 /*
-Dynamic programming
-Time: O(N^2) N: number of array
-Space: O(N)
+  Dynamic programming
+  Time: O(N^2) N: number of nums
+  Space: O(N)
 */
-function jump(nums) {
+function _jump(nums) {
   const jumps = new Array(nums.length).fill(Infinity);
   jumps[0] = 0;
 
@@ -22,7 +22,69 @@ function jump(nums) {
     }
   }
 
+  if(jumps[jumps.length - 1] === Infinity) return 0;
   return jumps[jumps.length - 1];
+}
+
+/*
+  Partial greedy solution
+*/
+function __jump(nums) {
+  //no jump available or needed
+  if(!nums[0] || nums.length === 1) return 0;
+
+  let farthest = 0;
+  let steps = 0;
+
+  for(let i = 0; i < nums.length; i++) {
+    if(i === 0) {
+      farthest = nums[0] + i;
+      steps = 1;
+    } else {
+
+      if(i + nums[i] > farthest) {
+        farthest = i + nums[i];
+        steps++;
+      } else if(nums[i] === 0 && i === farthest) {
+        return 0;
+      }
+
+    }
+
+    if(farthest >= nums.length - 1) {
+      return steps;
+    }
+  }
+
+  return 0;
+}
+
+/*
+  Time: O(N) N: number of nums
+  Space: O(1)
+*/
+function jump(nums) {
+  if(!nums[0] || nums.length === 1) return 0;
+  let begin = 0;
+  let currentMaxRange = nums[begin];
+  let jump = 1;
+
+  let nextMaxRange = 0;
+
+  while(currentMaxRange < nums.length - 1) {
+    
+    for(let i = begin + 1; i <= currentMaxRange; i++) {
+      nextMaxRange = Math.max(nextMaxRange, i + nums[i]);
+    }
+
+    begin = currentMaxRange;
+    currentMaxRange = nextMaxRange;
+    jump++;
+
+    nextMaxRange = 0;
+  }
+
+  return jump;
 }
 
 module.exports = {jump}
--- 45_jump-game-ii/jump-game-ii.test.js
+++ 45_jump-game-ii/jump-game-ii.test.js
@@ -16,3 +16,27 @@ Output: 2
 test("example 2", () => {
   expect(jump([2,3,0,1,4])).toEqual(2);
 });
+
+test("example 3", () => {
+  expect(jump([0,3,0,1,4])).toEqual(0);
+});
+
+test("example 4", () => {
+  expect(jump([1,2])).toEqual(1);
+});
+
+test("example 5", () => {
+  expect(jump([2,1])).toEqual(1);
+});
+
+test("example 6", () => {
+  expect(jump([1])).toEqual(0);
+});
+
+test("example 7", () => {
+  expect(jump([7,0,9,6,9,6,1,7,9,0,1,2,9,0,3])).toEqual(2);
+});
+
+test("example 8", () => {
+  expect(jump([1,1,1,1])).toEqual(3);
+});
